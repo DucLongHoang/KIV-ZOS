87d72d9f9fb5631210ee0de0e2ea786f4b846408
LongWindows <duclonghoang.dlh+github@gmail.com>
2022-12-06 08:55:01 +0100

    init_fs, fs functions

--- sp/FAT.cpp
+++ sp/FAT.cpp
@@ -38,8 +38,10 @@ void FAT::write_FAT(uint idx, int idxOrFlag) {
 }
 
 uint FAT::find_free_index() {
-    for (size_t i = 0; i < table.size(); ++i) {
-        if (table[i] == FAT::FLAG_UNUSED) return i;
+    // indexed range based for loop
+    for (size_t idx = 0; const auto& it : *this) {
+        if (it == FAT::FLAG_UNUSED) return idx;
+        ++idx;
     }
     return FAT::FLAG_NO_FREE_SPACE;
 }
@@ -66,10 +68,25 @@ bool FAT_Filesystem::init_fs(const std::vector<std::any>& args) {
     auto mode = std::ios::in | std::ios::out | std::ios::binary | std::ios::trunc;
     mFileStream.open(mDiskName, mode);
 
-    this->mBS.init(diskSize);
-    this->mFAT.init(mBS.mFatEntryCount);
-    this->mRootDir.init("/"s, false, 0, 0);
-    mFAT.write_FAT(0, FAT::FLAG_FILE_END);
+    // construct disk sections
+    mBS = std::make_unique<BootSector>();
+    mFAT = std::make_unique<FAT>();
+    mRootDir = std::make_unique<DirectoryItem>();
+
+    // init disk sections
+    this->mBS->init(diskSize);
+    this->mFAT->init(mBS->mFatEntryCount);
+    this->mRootDir->init("/"s, false, 0, 0);
+    mFAT->write_FAT(0, FAT::FLAG_FILE_END);
+
+    // init test file
+    DirectoryItem di;
+    std::string diContent{"This is content of test.txt. Do what you want with this information though."};
+    uint startCluster = mFAT->find_free_index();
+    di.init("test.txt", true, diContent.size(), startCluster);
+    mFAT->write_FAT(startCluster, FAT::FLAG_FILE_END);
+    // increase size by new item
+    mRootDir->mSize = di.size();
 
     if (!mFileStream) {
         std::cout << "Error opening file" << std::endl;
@@ -77,51 +94,46 @@ bool FAT_Filesystem::init_fs(const std::vector<std::any>& args) {
     }
 
     // write boot sector
-    string_to_stream(mFileStream, mBS.mSignature);
-    write_to_stream(mFileStream, mBS.mDiskSize);
-    write_to_stream(mFileStream, mBS.mClusterSize);
-    write_to_stream(mFileStream, mBS.mClusterCount);
-    write_to_stream(mFileStream, mBS.mFatEntryCount);
-    write_to_stream(mFileStream, mBS.mFatStartAddress);
-    write_to_stream(mFileStream, mBS.mDataStartAddress);
+    string_to_stream(mFileStream, mBS->mSignature);
+    write_to_stream(mFileStream, mBS->mDiskSize);
+    write_to_stream(mFileStream, mBS->mClusterSize);
+    write_to_stream(mFileStream, mBS->mClusterCount);
+    write_to_stream(mFileStream, mBS->mFatEntryCount);
+    write_to_stream(mFileStream, mBS->mFatStartAddress);
+    write_to_stream(mFileStream, mBS->mDataStartAddress);
 
     // write FAT
-    mFileStream.seekp(mBS.mFatStartAddress);
-    for_each(mFAT.table.begin(), mFAT.table.end(), [this](int fatEntry) {
+    mFileStream.seekp(mBS->mFatStartAddress);
+    for (auto fatEntry : *mFAT) {
         write_to_stream(mFileStream, fatEntry);
-    });
+    }
 
     // wipe all clusters
     std::array<char, CLUSTER_SIZE> cluster{};
     cluster.fill('\0');
-    mFileStream.seekp(mBS.mDataStartAddress);
-    for (size_t i = 0; i < mBS.mClusterCount; ++i) {
+    mFileStream.seekp(mBS->mDataStartAddress);
+    for (size_t i = 0; i < mBS->mClusterCount; ++i) {
         write_to_stream(mFileStream, cluster);
     }
 
     // write root dir to first cluster
-    mFileStream.seekp(mBS.mDataStartAddress);
-    string_to_stream(mFileStream, mRootDir.mFilename);
-    write_to_stream(mFileStream, mRootDir.mIsFile);
-    write_to_stream(mFileStream, mRootDir.mSize);
-    write_to_stream(mFileStream, mRootDir.mStartCluster);
-
-    DirectoryItem di;
-    std::string diContent{"This is content of test.txt. Do what you want with this information though."};
-    di.init("test.txt", true, diContent.size(), 1);
-    mFAT.write_FAT(1, FAT::FLAG_FILE_END);
+    uint ad1 = mBS->mDataStartAddress;
+    mFileStream.seekp(mBS->mDataStartAddress);
+    string_to_stream(mFileStream, mRootDir->mFilename);
+    write_to_stream(mFileStream, mRootDir->mIsFile);
+    write_to_stream(mFileStream, mRootDir->mSize);
+    write_to_stream(mFileStream, mRootDir->mStartCluster);
 
     // write file as file of root dir
-//    mFileStream.seekp(mBS.mDataStartAddress + CLUSTER_SIZE);
     string_to_stream(mFileStream, di.mFilename);
     write_to_stream(mFileStream, di.mIsFile);
     write_to_stream(mFileStream, di.mSize);
     write_to_stream(mFileStream, di.mStartCluster);
 
     // write file to second cluster
-    mFileStream.seekp(mBS.mDataStartAddress + CLUSTER_SIZE);
+    uint ad2 = mBS->mDataStartAddress + CLUSTER_SIZE * startCluster;
+    mFileStream.seekp(mBS->mDataStartAddress + CLUSTER_SIZE * startCluster);
     string_to_stream(mFileStream, diContent);
-
 
     mFileStream.flush();
     return true;
@@ -131,11 +143,16 @@ bool FAT_Filesystem::mount_fs(const std::vector<std::any>& args) {
     auto mode = std::ios::in | std::ios::out | std::ios::binary | std::ios::ate;
     mFileStream.open(mDiskName, mode);
 
+    // construct disk sections
+    mBS = std::make_unique<BootSector>();
+    mFAT = std::make_unique<FAT>();
+    mRootDir = std::make_unique<DirectoryItem>();
+
     // read info from disk and init
-    mBS.init_from_disk(mFileStream, 0);
-    mFAT.init(mBS.mFatEntryCount);
-    mFAT.init_from_disk(mFileStream, mBS.mFatStartAddress);
-    mRootDir.init_from_disk(mFileStream, mBS.mDataStartAddress);
+    mBS->init_from_disk(mFileStream, 0);
+    mFAT->init(mBS->mFatEntryCount);
+    mFAT->init_from_disk(mFileStream, mBS->mFatStartAddress);
+    mRootDir->init_from_disk(mFileStream, mBS->mDataStartAddress);
 
     return true;
 }
@@ -143,23 +160,56 @@ bool FAT_Filesystem::mount_fs(const std::vector<std::any>& args) {
 uint FAT_Filesystem::fs_creat(const std::vector<std::any>& args) {
     auto filename{any_cast<std::string>(args[0])};
     auto isFile{any_cast<bool>(args[1])};
-    uint size;
-    uint startCluster = mFAT.find_free_index();
+    auto size{any_cast<uint>(args[2])};
+    uint startCluster = mFAT->find_free_index();
 
-    uint address = mBS.mDataStartAddress + (startCluster * CLUSTER_SIZE);
-    mFAT.write_FAT(startCluster, FAT::FLAG_FILE_END);
+    uint address = mBS->mDataStartAddress + (startCluster * CLUSTER_SIZE);
+    mFAT->write_FAT(startCluster, FAT::FLAG_FILE_END);
 
-    return false;
+    return true;
 }
 
 uint FAT_Filesystem::fs_open(const std::vector<std::any>& args) {
+    // method takes in name of file,
 
-    return false;
+    if (args.empty()) {
+        return 0;
+    }
+
+    return 500;
 }
 
-uint FAT_Filesystem::fs_read(const std::vector<std::any>& args) {
+uint FAT_Filesystem::fs_read(std::vector<std::any>& args) {
+    auto cluster = any_cast<uint>(args[0]);
+    auto buffer = any_cast<std::vector<char>>(args[1]);
 
-    return false;
+    if (mFAT->table[cluster] > 0) {
+        // recursion
+
+    }
+
+    if (mFAT->table[cluster] == FAT::FLAG_FILE_END) {
+        uint ad1 = mBS->mDataStartAddress + (CLUSTER_SIZE * cluster);
+//        mFileStream.seekg(mBS->mDataStartAddress + (CLUSTER_SIZE * cluster) + mRootDir->size());
+//        read_from_stream(mFileStream, buffer);
+        DirectoryItem di;
+        di.init_from_disk(mFileStream, ad1);
+
+        DirectoryItem di2;
+        di2.mFilename = string_from_stream(mFileStream, 12);
+        read_from_stream(mFileStream, di2.mIsFile);
+        read_from_stream(mFileStream, di2.mSize);
+        read_from_stream(mFileStream, di2.mStartCluster);
+
+        mFileStream.seekp(ad1);
+        mFileStream.read(buffer.data(), CLUSTER_SIZE);
+
+        mFileStream.seekp(ad1 + CLUSTER_SIZE);
+        mFileStream.read(buffer.data(), CLUSTER_SIZE);
+
+    }
+
+    return buffer.size();
 }
 
 uint FAT_Filesystem::fs_write(const std::vector<std::any>& args) {
@@ -169,13 +219,11 @@ uint FAT_Filesystem::fs_write(const std::vector<std::any>& args) {
 
 
     std::cout << "Writing: ";
-
-    std::for_each(args.begin(), args.end(), [this, args](const std::any& a) {
-        std::string str{any_cast<std::string>(a)};
+    for (const auto& it : args) {
+        std::string str{any_cast<std::string>(it)};
         std::cout << str << " ";
         string_to_stream(mFileStream, str);
-    });
-
+    }
     std::cout << std::endl;
     mFileStream.flush();
 
